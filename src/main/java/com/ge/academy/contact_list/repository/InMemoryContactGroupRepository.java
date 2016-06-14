0b792eeb68029b34182e20d5c2a8b4588f4eb8c6
Aron Horvath <aron.horvath@ge.com>
2016-06-14 17:22:41 +0200

    Merge with master

--- src/main/java/com/ge/academy/contact_list/repository/InMemoryContactGroupRepository.java
+++ src/main/java/com/ge/academy/contact_list/repository/InMemoryContactGroupRepository.java
@@ -5,32 +5,59 @@ import com.ge.academy.contact_list.entity.ContactGroupId;
 import com.ge.academy.contact_list.exception.EntityNotFoundException;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryContactGroupRepository implements ContactGroupRepository {
+
+    private ConcurrentMap<ContactGroupId, ContactGroup> contactGroups = new ConcurrentHashMap<>();
+
+
     @Override
     public ContactGroup save(ContactGroup contactGroup) throws EntityNotFoundException {
-        return null;
+        contactGroups.put(contactGroup.getId(), contactGroup);
+        return contactGroup;
     }
 
     @Override
     public void delete(ContactGroupId contactGroupId) throws EntityNotFoundException {
+        if (contactGroups.get(contactGroupId) == null) {
+            throw new EntityNotFoundException(contactGroupId.getClass(), contactGroupId);
+        } else {
+            contactGroups.remove(contactGroupId);
+        }
 
     }
 
     @Override
     public ContactGroup findOne(ContactGroupId contactGroupId) throws EntityNotFoundException {
-        return null;
+        if (contactGroups.get(contactGroupId) == null) {
+            throw new EntityNotFoundException(contactGroupId.getClass(), contactGroupId);
+        } else {
+            final ContactGroup managed = contactGroups.get(contactGroupId);
+            return managed;
+        }
     }
 
     @Override
     public List<ContactGroup> findAll() {
-        return null;
+        return contactGroups.values().stream().map(mapped -> new ContactGroup(mapped)).collect(Collectors.toList());
     }
 
     @Override
     public List<ContactGroup> findByOwner(String s) {
-        return null;
+
+        List<ContactGroup> ownersContactGroups = new ArrayList<>();
+        for (ContactGroup contactGroup : contactGroups.values()) {
+            if (contactGroup.getId().getUserName().equals(s)) {
+                ownersContactGroups.add(new ContactGroup(contactGroup));
+            }
+        }
+
+        return ownersContactGroups;
     }
 }
